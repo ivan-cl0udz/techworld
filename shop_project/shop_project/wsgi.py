1741c6b656ce9b2051026cff4701a4a18d5999f9
ivan-cl0udz <ivanfilipets115@gmail.com>
2025-10-24 17:04:31 +0100

    Update wsgi.py

--- shop_project/shop_project/wsgi.py
+++ shop_project/shop_project/wsgi.py
@@ -12,5 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop_project.settings')
-
+ALLOWED_HOSTS = ['techworld.onrender.com']
 application = get_wsgi_application()
